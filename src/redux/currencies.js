86ffa83b740f281677a981f6a9438407f329dcf9
anyepeter <petzyrockchendi@gmail.com>
2023-01-02 12:06:06 -0800

    create the action and reducer

---
+++ src/redux/currencies.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolKit';
+import { fetchCurrency } from './coinAction';
+
+const initialState = {
+    currencies: [],
+    status: null,
+}
+
+const currencyReducer = createSlice({
+    name:'currency',
+    initialState,
+    extraReducers: (builder) => {
+        builder.addCase(fetchCurrency.fulfilled, (state, action) => {
+            const items = state;
+            items.status = 'success';
+            items.currencies = action.payload;
+        })
+    }
+}
+)
+
+export default currencyReducer.reducer;
